d37808d8f58a93fd7a132d30b5b64bfbf786820b
santiago campos <campos.santiago138@gmail.com>
2023-02-05 13:08:15 -0500

    tuples, list, string,dictionaries

--- basics.py
+++ basics.py
@@ -3,3 +3,27 @@ week_days = 7
 
 week_hours = day_hours * week_days
 print(week_hours) 
+
+
+x = 10
+y ="10"
+z = 10.1
+
+sum1 = x + x
+sum2 = y + y
+
+print(sum1, sum2)
+print(type(x), type(y), type(z))
+
+student_grades = [9.1, 8.8, 7.5]
+# student_grades = {"Mary": 9.1, "Sim": 8.8, "John": 7.5}
+
+mysum = sum(student_grades)
+length = len(student_grades)
+mean = mysum / length
+print(mean)
+# print(temperatures)
+
+
+monday_temperature = (1, 4, 5)
+print(monday_temperature)
--- index.py
+++ index.py
@@ -3,3 +3,6 @@ print(pract)
 print(24 * 7)
 
 # fsdgklj
+import datetime
+x = datetime.datetime.now()
+print(x)
