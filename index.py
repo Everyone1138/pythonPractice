9ca1143bbd3d63e8a28022d0f648f8d47d51a6d4
santiago campos <campos.santiago138@gmail.com>
2023-02-06 21:27:51 -0500

    daily python: data types

--- index.py
+++ index.py
@@ -6,3 +6,36 @@ print(24 * 7)
 import datetime
 x = datetime.datetime.now()
 print(x)
+
+
+mood = "this is a string"
+strength = 1.99
+rank = 10
+
+x = 4
+y = 6
+z = 10
+s = x + y + z
+print(s)
+
+
+temperature = [10.99, 6, "strings are cool"]
+print(temperature)
+
+rainfall = [20.99, 13, "this is also a string", [1, 2, 3,4]]
+print(rainfall)
+
+student_grades=[9.1,8.8,7.5]
+
+max_value= sum(student_grades)
+
+print(max_value)
+
+# student_grades2 = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+
+# print(student_grades2(10.0))
+userName = "Python3"
+
+print(userName.lower())
+
+
