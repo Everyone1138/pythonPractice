8ebb72be4548e051241f902635483cba6087cc9c
santiago campos <campos.santiago138@gmail.com>
2023-02-19 22:31:12 -0500

    user input in python

--- mean.py
+++ mean.py
@@ -47,3 +47,14 @@ if "hello" in message:
     print("hi")
 else:
     print("I don't understand")
+
+
+def weather_condition(temperature):
+    if temperature > 7:
+        return "Warm"
+    else:
+        return "Cold"
+
+user_input = float(input("Enter temperature:"))
+print(weather_condition(user_input))
+
