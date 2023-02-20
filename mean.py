07f0d05a533d7198b9d4937b9a2edd58a0c6979e
santiago campos <campos.santiago138@gmail.com>
2023-02-19 22:16:18 -0500

    daily commit: python

--- mean.py
+++ mean.py
@@ -6,5 +6,44 @@ def meaN(value):
 
     return the_mean
 
+monday_temperatures = [8.8, 9.1, 9.9]
 student_grades = {"Marry": 9.1,"Sim": 8.8, "John": 7.5}
 print(meaN(student_grades))
+print(meaN(monday_temperatures))
+
+
+x = 3
+y = 1
+if x > y:
+    print("X is greater than Y")
+elif x == y:
+    print("X is equal to Y")
+else:
+    print("X is less than Y")
+
+
+if      3 > 1:
+    print('a')
+
+print('aa')
+print('aaa')
+
+if 3>1:
+    print('b')
+
+print('bb')
+print('bbb')
+
+if 3 > 1:
+    print('c')
+
+print('cc')
+print('ccc')
+
+
+message = "hello there"
+
+if "hello" in message:
+    print("hi")
+else:
+    print("I don't understand")
